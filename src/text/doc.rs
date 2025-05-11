88405b154500c600a781f2126324a72653943017
Zorbn <public.zorb@gmail.com>
2025-05-10 23:30:00 -0700

    Improve LSP doc state tracking

--- src/text/doc.rs
+++ src/text/doc.rs
@@ -1,3 +1,5 @@
+mod lsp;
+
 use std::{
     collections::HashMap,
     fmt::Display,
@@ -9,10 +11,9 @@ use std::{
 };
 
 use crate::{
-    config::language::Language,
     ctx::{ctx_with_time, Ctx},
     geometry::{position::Position, rect::Rect, visual_position::VisualPosition},
-    lsp::{language_server::LanguageServer, types::TextEdit, LspExpectedResponse, LspSentRequest},
+    lsp::{types::TextEdit, LspExpectedResponse},
     normalizable::Normalizable,
     platform::gfx::Gfx,
     temp_buffer::TempString,
@@ -129,6 +130,7 @@ pub struct Doc {
     needs_tokenization: bool,
 
     lsp_expected_responses: HashMap<&'static str, LspExpectedResponse>,
+    lsp_is_open: bool,
 
     kind: DocKind,
 }
@@ -175,6 +177,7 @@ impl Doc {
             needs_tokenization: false,
 
             lsp_expected_responses: HashMap::new(),
+            lsp_is_open: false,
 
             kind,
         };
@@ -1529,7 +1532,7 @@ impl Doc {
             ctx.buffers.lines.push(line);
         }
 
-        self.lsp_text_document_notification("textDocument/didClose", ctx);
+        self.lsp_did_close(ctx);
     }
 
     pub fn is_change_unexpected(&mut self) -> bool {
@@ -2046,245 +2049,6 @@ impl Doc {
                 future_edit.range = (future_start, future_end);
             }
         }
-    }
-
-    pub fn get_language_server_mut<'a>(
-        &self,
-        ctx: &'a mut Ctx,
-    ) -> Option<(&'a Language, &'a mut LanguageServer)> {
-        if self.kind == DocKind::Output {
-            return None;
-        }
-
-        let language = ctx.config.get_language_for_doc(self)?;
-        let language_server = ctx.lsp.get_language_server_mut(language)?;
-
-        Some((language, language_server))
-    }
-
-    fn lsp_add_expected_response(
-        &mut self,
-        sent_request: LspSentRequest,
-        position: Option<Position>,
-    ) {
-        self.lsp_expected_responses.insert(
-            sent_request.method,
-            LspExpectedResponse {
-                id: sent_request.id,
-                position,
-                version: self.version,
-            },
-        );
-    }
-
-    pub fn lsp_is_response_expected(
-        &mut self,
-        method: &str,
-        id: Option<usize>,
-        ctx: &mut Ctx,
-    ) -> bool {
-        let Some(id) = id else {
-            // This was a notification so it's expected by default.
-            return true;
-        };
-
-        let Some(expected_response) = self.lsp_expected_responses.get(method).copied() else {
-            // Expected responses don't need to be tracked for this method.
-            return true;
-        };
-
-        if expected_response.id != id {
-            return false;
-        }
-
-        self.lsp_expected_responses.remove(method);
-
-        let position = self.get_cursor(CursorIndex::Main).position;
-
-        let is_position_expected = expected_response
-            .position
-            .is_none_or(|expected_position| expected_position == position);
-
-        let is_version_expected = expected_response.version == self.version;
-
-        if !is_position_expected || !is_version_expected {
-            // We received the expected response, but the doc didn't match the expected state.
-            if method == "textDocument/completion" {
-                self.lsp_completion(ctx);
-            }
-
-            if method == "textDocument/diagnostic" {
-                self.lsp_diagnostic(ctx);
-            }
-
-            return false;
-        }
-
-        true
-    }
-
-    pub fn lsp_did_open(&mut self, text: &str, ctx: &mut Ctx) -> Option<()> {
-        let (language, language_server) = self.get_language_server_mut(ctx)?;
-        let language_id = language.lsp_language_id.as_ref()?;
-        let path = self.path.some()?;
-
-        language_server.did_open(path, language_id, self.version, text);
-        self.lsp_diagnostic(ctx);
-
-        Some(())
-    }
-
-    fn lsp_did_change(
-        &mut self,
-        start: Position,
-        end: Position,
-        text: &str,
-        ctx: &mut Ctx,
-    ) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-
-        language_server.did_change(path, self.version, start, end, text, self);
-
-        Some(())
-    }
-
-    fn lsp_diagnostic(&mut self, ctx: &mut Ctx) -> Option<()> {
-        if self
-            .lsp_expected_responses
-            .contains_key("textDocument/diagnostic")
-        {
-            return None;
-        }
-
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-
-        let sent_request = language_server.diagnostic(path)?;
-        self.lsp_add_expected_response(sent_request, None);
-
-        Some(())
-    }
-
-    pub fn lsp_completion(&mut self, ctx: &mut Ctx) -> Option<()> {
-        if self
-            .lsp_expected_responses
-            .contains_key("textDocument/completion")
-        {
-            return None;
-        }
-
-        self.get_completion_prefix(ctx.gfx)?;
-
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-        let position = self.get_cursor(CursorIndex::Main).position;
-
-        let sent_request = language_server.completion(path, position, self);
-        self.lsp_add_expected_response(sent_request, Some(position));
-
-        Some(())
-    }
-
-    pub fn lsp_code_action(&mut self, ctx: &mut Ctx) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-
-        let cursor = self.get_cursor(CursorIndex::Main);
-
-        let (start, end) = if let Some(selection) = cursor.get_selection() {
-            (selection.start, selection.end)
-        } else {
-            (cursor.position, cursor.position)
-        };
-
-        let sent_request = language_server.code_action(path, start, end, self);
-        self.lsp_add_expected_response(sent_request, Some(cursor.position));
-
-        Some(())
-    }
-
-    pub fn lsp_prepare_rename(&mut self, ctx: &mut Ctx) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-        let position = self.get_cursor(CursorIndex::Main).position;
-
-        let sent_request = language_server.prepare_rename(path, position, self);
-        self.lsp_add_expected_response(sent_request, Some(position));
-
-        Some(())
-    }
-
-    pub fn lsp_rename(&self, new_name: &str, ctx: &mut Ctx) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-        let position = self.get_cursor(CursorIndex::Main).position;
-
-        language_server.rename(new_name, path, position, self);
-
-        Some(())
-    }
-
-    pub fn lsp_references(&mut self, ctx: &mut Ctx) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-        let position = self.get_cursor(CursorIndex::Main).position;
-
-        let sent_request = language_server.references(path, position, self);
-        self.lsp_add_expected_response(sent_request, Some(position));
-
-        Some(())
-    }
-
-    pub fn lsp_definition(&mut self, position: Position, ctx: &mut Ctx) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-
-        let sent_request = language_server.definition(path, position, self);
-        self.lsp_add_expected_response(sent_request, None);
-
-        Some(())
-    }
-
-    pub fn lsp_signature_help(
-        &mut self,
-        trigger_char: Option<char>,
-        is_retrigger: bool,
-        ctx: &mut Ctx,
-    ) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-        let position = self.get_cursor(CursorIndex::Main).position;
-
-        let sent_request =
-            language_server.signature_help(path, position, trigger_char, is_retrigger, self);
-        self.lsp_add_expected_response(sent_request, Some(position));
-
-        Some(())
-    }
-
-    pub fn lsp_formatting(&mut self, ctx: &mut Ctx) -> Option<()> {
-        let indent_width = ctx.config.get_indent_width_for_doc(self);
-
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-
-        language_server.formatting(path, indent_width);
-
-        Some(())
-    }
-
-    fn lsp_text_document_notification(
-        &mut self,
-        method: &'static str,
-        ctx: &mut Ctx,
-    ) -> Option<()> {
-        let (_, language_server) = self.get_language_server_mut(ctx)?;
-        let path = self.path.some()?;
-
-        language_server.text_document_notification(path, method);
-
-        Some(())
     }
 
     pub fn version(&self) -> usize {
---
+++ src/text/doc/lsp.rs
@@ -0,0 +1,307 @@
+use crate::{
+    config::language::Language,
+    ctx::Ctx,
+    geometry::position::Position,
+    lsp::{language_server::LanguageServer, LspExpectedResponse, LspSentRequest},
+};
+
+use crate::text::cursor_index::CursorIndex;
+
+use super::{Doc, DocKind};
+
+impl Doc {
+    pub fn get_language_server_mut<'a>(
+        &self,
+        ctx: &'a mut Ctx,
+    ) -> Option<(&'a Language, &'a mut LanguageServer)> {
+        if self.kind == DocKind::Output {
+            return None;
+        }
+
+        let language = ctx.config.get_language_for_doc(self)?;
+        let language_server = ctx.lsp.get_language_server_mut(language)?;
+
+        Some((language, language_server))
+    }
+
+    fn lsp_add_expected_response(
+        &mut self,
+        sent_request: LspSentRequest,
+        position: Option<Position>,
+    ) {
+        self.lsp_expected_responses.insert(
+            sent_request.method,
+            LspExpectedResponse {
+                id: sent_request.id,
+                position,
+                version: self.version,
+            },
+        );
+    }
+
+    pub fn lsp_is_response_expected(
+        &mut self,
+        method: &str,
+        id: Option<usize>,
+        ctx: &mut Ctx,
+    ) -> bool {
+        let Some(id) = id else {
+            // This was a notification so it's expected by default.
+            return true;
+        };
+
+        let Some(expected_response) = self.lsp_expected_responses.get(method).copied() else {
+            // Expected responses don't need to be tracked for this method.
+            return true;
+        };
+
+        if expected_response.id != id {
+            return false;
+        }
+
+        self.lsp_expected_responses.remove(method);
+
+        let position = self.get_cursor(CursorIndex::Main).position;
+
+        let is_position_expected = expected_response
+            .position
+            .is_none_or(|expected_position| expected_position == position);
+
+        let is_version_expected = expected_response.version == self.version;
+
+        if !is_position_expected || !is_version_expected {
+            // We received the expected response, but the doc didn't match the expected state.
+            if method == "textDocument/completion" {
+                self.lsp_completion(ctx);
+            }
+
+            if method == "textDocument/diagnostic" {
+                self.lsp_diagnostic(ctx);
+            }
+
+            return false;
+        }
+
+        true
+    }
+
+    pub fn lsp_did_open(&mut self, text: &str, ctx: &mut Ctx) -> Option<()> {
+        if self.lsp_is_open {
+            return None;
+        }
+
+        let (language, language_server) = self.get_language_server_mut(ctx)?;
+        let language_id = language.lsp_language_id.as_ref()?;
+        let path = self.path.some()?;
+
+        language_server.did_open(path, language_id, self.version, text);
+        self.lsp_diagnostic(ctx);
+
+        self.lsp_is_open = true;
+
+        Some(())
+    }
+
+    pub fn lsp_did_close(&mut self, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        self.lsp_text_document_notification("textDocument/didClose", ctx)?;
+
+        self.lsp_is_open = false;
+
+        Some(())
+    }
+
+    pub fn lsp_did_change(
+        &mut self,
+        start: Position,
+        end: Position,
+        text: &str,
+        ctx: &mut Ctx,
+    ) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+
+        language_server.did_change(path, self.version, start, end, text, self);
+
+        Some(())
+    }
+
+    pub fn lsp_diagnostic(&mut self, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open
+            || self
+                .lsp_expected_responses
+                .contains_key("textDocument/diagnostic")
+        {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+
+        let sent_request = language_server.diagnostic(path)?;
+        self.lsp_add_expected_response(sent_request, None);
+
+        Some(())
+    }
+
+    pub fn lsp_completion(&mut self, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open
+            || self
+                .lsp_expected_responses
+                .contains_key("textDocument/completion")
+        {
+            return None;
+        }
+
+        self.get_completion_prefix(ctx.gfx)?;
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+        let position = self.get_cursor(CursorIndex::Main).position;
+
+        let sent_request = language_server.completion(path, position, self);
+        self.lsp_add_expected_response(sent_request, Some(position));
+
+        Some(())
+    }
+
+    pub fn lsp_code_action(&mut self, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+
+        let cursor = self.get_cursor(CursorIndex::Main);
+
+        let (start, end) = if let Some(selection) = cursor.get_selection() {
+            (selection.start, selection.end)
+        } else {
+            (cursor.position, cursor.position)
+        };
+
+        let sent_request = language_server.code_action(path, start, end, self);
+        self.lsp_add_expected_response(sent_request, Some(cursor.position));
+
+        Some(())
+    }
+
+    pub fn lsp_prepare_rename(&mut self, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+        let position = self.get_cursor(CursorIndex::Main).position;
+
+        let sent_request = language_server.prepare_rename(path, position, self);
+        self.lsp_add_expected_response(sent_request, Some(position));
+
+        Some(())
+    }
+
+    pub fn lsp_rename(&self, new_name: &str, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+        let position = self.get_cursor(CursorIndex::Main).position;
+
+        language_server.rename(new_name, path, position, self);
+
+        Some(())
+    }
+
+    pub fn lsp_references(&mut self, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+        let position = self.get_cursor(CursorIndex::Main).position;
+
+        let sent_request = language_server.references(path, position, self);
+        self.lsp_add_expected_response(sent_request, Some(position));
+
+        Some(())
+    }
+
+    pub fn lsp_definition(&mut self, position: Position, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+
+        let sent_request = language_server.definition(path, position, self);
+        self.lsp_add_expected_response(sent_request, None);
+
+        Some(())
+    }
+
+    pub fn lsp_signature_help(
+        &mut self,
+        trigger_char: Option<char>,
+        is_retrigger: bool,
+        ctx: &mut Ctx,
+    ) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+        let position = self.get_cursor(CursorIndex::Main).position;
+
+        let sent_request =
+            language_server.signature_help(path, position, trigger_char, is_retrigger, self);
+        self.lsp_add_expected_response(sent_request, Some(position));
+
+        Some(())
+    }
+
+    pub fn lsp_formatting(&mut self, ctx: &mut Ctx) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let indent_width = ctx.config.get_indent_width_for_doc(self);
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+
+        language_server.formatting(path, indent_width);
+
+        Some(())
+    }
+
+    pub fn lsp_text_document_notification(
+        &mut self,
+        method: &'static str,
+        ctx: &mut Ctx,
+    ) -> Option<()> {
+        if !self.lsp_is_open {
+            return None;
+        }
+
+        let (_, language_server) = self.get_language_server_mut(ctx)?;
+        let path = self.path.some()?;
+
+        language_server.text_document_notification(path, method);
+
+        Some(())
+    }
+}
